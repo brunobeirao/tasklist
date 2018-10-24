0071ed6c24eb510663f82dfd85acbfa274e55fc4
brunobeirao <bruno.silva@portalunimed.com.br>
2018-10-24 15:56:31 -0200

    Desenvolvimeto frontend

--- tasklist-front/js/controller.js
+++ tasklist-front/js/controller.js
@@ -1,6 +1,8 @@
 tasklist.controller('TasklistController', function($scope, tasklistService) {
 
-	var ctrl = this;
+    var ctrl = this;
+    $scope.enabledEdit=[];
+    $scope.enabledSalvar=[];
 
 	tasklistService.getTasks(function(response){
         ctrl.getTasks = response;
@@ -14,11 +16,30 @@ tasklist.controller('TasklistController', function($scope, tasklistService) {
 
     };
 
-    $scope.update = function(taskData) {
+    $scope.addTask = function(taskData) {
         tasklistService.addTask(taskData).success(function(response){  
         }).error(function(data, status) {
             console.error('Repos error', status, data);
         });
+    };
+    
+    $scope.editarTask = function(index){
+        $scope.enabledEdit[index] = true;
+        $scope.enabledSalvar[index] = true;
+    };
+
+    $scope.salvarEdicaoTask = function(taskData){
+        console.log(taskData);
+        tasklistService.updateTask(taskData).success(function(response){  
+        }).error(function(data, status) {
+            console.error('Repos error', status, data);
+        });
+    };
+    $scope.deleteTask = function(id) {
+        tasklistService.deleteTask(id).success(function(response){  
+        }).error(function(data, status) {
+            console.error('Repos error', status, data);
+        });
       };
- 
+    
 });
--- tasklist-front/js/services.js
+++ tasklist-front/js/services.js
@@ -14,11 +14,15 @@ tasklist.service('tasklistService', function ($http, $resource){
 	
 	var ADD_TASK = 'http://localhost:8080/tasklist/add';
 	
-	var LIST_TASK = 'http://localhost:8080/tasklist/list';
+    var LIST_TASK = 'http://localhost:8080/tasklist/list';
+    
+    var UPDATE_TASK = 'http://localhost:8080/tasklist/update';
 
     var serviceAddTask = $resource(ADD_TASK, {titulo:'@titulo', descricao:'@descricao', status:'@status'}, queryConfig);
 	
-	var serviceGetTask = $resource(LIST_TASK, queryConfig);
+    var serviceGetTask = $resource(LIST_TASK, queryConfig);
+    
+    var servicePutTask = $resource(UPDATE_TASK, queryConfig);
 	
 	var _addTask = function (taskData, callback, error) {
         console.log(taskData);
@@ -28,6 +32,22 @@ tasklist.service('tasklistService', function ($http, $resource){
             method: 'POST',
             transformRequest: transformRequest,
             data:{
+                titulo: taskData.titulo,
+                descricao: taskData.descricao,
+                status: taskData.status
+            }
+        });
+    };
+
+    var _updateTask = function (taskData, callback, error) {
+        console.log(taskData);
+        return $http ({
+            url : UPDATE_TASK,
+            headers : headers,
+            method: 'PUT',
+            transformRequest: transformRequest,
+            data:{
+                id: taskData.id,
                 titulo: taskData.titulo,
                 descricao: taskData.descricao,
                 status: taskData.status
@@ -52,7 +72,8 @@ tasklist.service('tasklistService', function ($http, $resource){
 
     return {
         addTask         		     	: _addTask,
-		getTasks						: _getTasks
+        getTasks						: _getTasks,
+        updateTask                      : _updateTask
     };
 
 });
